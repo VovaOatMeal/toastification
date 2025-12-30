994fc21c4246174804ac1c3dc24c8218554872a3
VovaOatMeal <shypik555@gmail.com>
2025-12-30 09:00:51 +0000

    refactor: replace iconsax_flutter icons with Material Icons in package's lib

--- lib/src/built_in/toastification_type.dart
+++ lib/src/built_in/toastification_type.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:iconsax_flutter/iconsax_flutter.dart';
 import 'package:toastification/src/utils/constants_values.dart';
 
 /// Defines the visual and behavioral characteristics of built-in toast notifications.
@@ -48,19 +47,19 @@ class ToastificationType {
 
   /// Predefined information toast type with blue color scheme and info icon
   static const info =
-      ToastificationType._('info', infoColor, Iconsax.info_circle_copy);
+      ToastificationType._('info', infoColor, Icons.info_outline);
 
   /// Predefined success toast type with green color scheme and checkmark icon
-  static const success =
-      ToastificationType._('success', successColor, Iconsax.tick_circle_copy);
+  static const success = ToastificationType._(
+      'success', successColor, Icons.check_circle_outline_outlined);
 
   /// Predefined warning toast type with yellow/orange color scheme and warning icon
   static const warning =
-      ToastificationType._('warning', warningColor, Iconsax.danger_copy);
+      ToastificationType._('warning', warningColor, Icons.warning_amber);
 
   /// Predefined error toast type with red color scheme and close icon
   static const error =
-      ToastificationType._('error', errorColor, Iconsax.close_circle_copy);
+      ToastificationType._('error', errorColor, Icons.error_outline);
 
   /// Creates a custom toast type with specified name, color and icon.
   ///
